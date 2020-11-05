d8d439b5b3d0702febf74c898ffa1a58b5e8399f
Isabel Perez <isabel.perez.ruiz25@gmail.com>
2020-11-05 23:07:21 +0100

    sprite player

--- js/player.js
+++ js/player.js
@@ -9,10 +9,20 @@ class Player {
       w: playerWidth,
       h: playerHeight
     },
-    this.imageName = playerImage
+    // this.imageName = playerImage
     this.playerInstance = undefined
     this.playerSpeed = 20
-    this.init()
+    this.image = new Image();
+    this.image.src = playerImage;
+    this.image.framesIndex = 0
+    this.image.frames = 3
+    this.image.width = 382
+    this.image.height = 132
+    // this.posX = playerPosX
+    // this.posY = playerPosY
+    this.width = playerWidth;
+    this.height = playerHeight;
+    // this.init()
     
   }
   init() {
@@ -21,10 +31,30 @@ class Player {
     
   }
 
-  draw() {
-    this.ctx.drawImage(this.playerInstance, this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h)
+  draw(frames) {
+    const dx = this.image.framesIndex * Math.floor(this.image.width / this.image.frames)
+    const dy = 0
+    const dwidth = Math.floor(this.image.width / this.image.frames)
+    const dheight = this.image.height
+    const sx= this.playerPos.x
+    const sy= this.playerPos.y
+    const swidth = this.playerSize.w
+    const sheight = this.playerSize.h
+
+    this.ctx.drawImage(this.image, dx, dy ,dwidth, dheight, sx, sy, swidth, sheight)
+    this.animate(frames)
+
+    // this.ctx.drawImage(this.playerInstance, this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h)
+
   }
-  
+  animate(frames) {
+    if (frames % 5 == 0) {
+      this.image.framesIndex++;
+    }
+    if (this.image.framesIndex > this.image.frames - 1) {
+      this.image.framesIndex = 0;
+    }
+  }
   move(dir) {
 
       dir === 'top' ? this.playerPos.y -= this.playerSpeed : null; 
@@ -35,3 +65,55 @@ class Player {
     }
   
 }
+// class Player {
+// constructor(ctx, playerPosX, playerPosY, playerWidth, playerHeight, playerImage) {
+//   this.ctx = ctx
+//   this.playerPos = {
+//     x: playerPosX,
+//     y: playerPosY
+//   },
+//   this.playerSize = {
+//     w: playerWidth,
+//     h: playerHeight
+//   },
+//   this.imageName = playerImage
+//   this.playerInstance = undefined
+//   this.playerSpeed = 20
+//   this.init()
+  
+// }
+// init() {
+//   this.playerInstance = new Image()
+//   this.playerInstance.src = `img/${this.imageName}`
+//   this.playerInstance.frames = 3;
+//   this.playerInstance.framesIndex = 0;
+  
+// }
+
+// draw(frames) {
+//   this.ctx.drawImage(this.playerInstance, this.playerInstance.framesIndex * Math.floor(this.playerSize.w / this.playerInstance.frames),
+//   0, Math.floor(this.playerSize.w / this.playerInstance.frames),this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h)
+//   this.animate(frames)
+//   this.move(dir)
+
+// }
+// animate(frames) {
+//   if (frames % 5 == 0) {
+//     this.playerInstance.framesIndex++;
+//   }
+//   if (this.playerInstance.framesIndex > this.playerInstance.frames - 1) {
+//     this.playerInstance.framesIndex = 0;
+//   }
+// }
+
+
+// move(dir) {
+
+//     dir === 'top' ? this.playerPos.y -= this.playerSpeed : null; 
+//     dir === 'bottom' ? this.playerPos.y += this.playerSpeed  : null;
+//     dir === 'left' ? this.playerPos.x -= this.playerSpeed  : null;
+//     dir === 'right' ? this.playerPos.x += this.playerSpeed : null;
+
+//   }
+
+// }
--- js/popino.js
+++ js/popino.js
@@ -1,5 +1,5 @@
 class Popino {
-    constructor(ctx, popinoPosX, popinoPosY, popinoWidth, popinoHeight, popinoImage) {
+    constructor(ctx, popinoPosX, popinoPosY, popinoWidth, popinoHeight, popinoImage, speed, direction) {
       this.ctx = ctx
       this.popinoPos = {
         x: popinoPosX,
@@ -13,6 +13,9 @@ class Popino {
       this.popinoInstance = undefined
       this.init()
       
+      this.speed = speed
+      this.direction = direction
+      
     }
     init() {
       this.popinoInstance = new Image()
@@ -23,5 +26,22 @@ class Popino {
     draw() {
       this.ctx.drawImage(this.popinoInstance, this.popinoPos.x, this.popinoPos.y, this.popinoSize.w, this.popinoSize.h)
     }
+  
+  move(x, y) {
+    setTimeout(() => {
+      if (this.popinoPos.y < y) {
+        this.popinoPos.y += this.speed;
+      }
+      if (this.popinoPos.y >= y && this.popinoPos.x < x) {
+        this.popinoPos.x += this.speed;
+      }
+    }, 20);
+    if (this.popinoPos.x > (x-10) && this.popinoPos.y > (y-10)) {
+        
+        clearInterval(this.interval);
+        document.querySelector(".win").classList.toggle("hidden")
+        document.querySelector("#myCanvas").classList.toggle("hidden")
+    }
+  }
     
   }
